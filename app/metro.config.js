7d7eb615c227b78e819bd0df7f12965bc3852ea5
Noah <noah.stolmaker@streem.com>
2022-10-06 15:01:45 -0700

    incremental commit. still throwing errors about not being able to find stuff

--- app/metro.config.js
+++ app/metro.config.js
@@ -1,4 +1,5 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const exclusionList = require("metro-config/src/defaults/exclusionList");
 
 const config = getDefaultConfig(__dirname);
 
@@ -8,19 +9,26 @@ module.exports = config;
 const path = require("path");
 
 // module.exports = createMetroConfiguration(__dirname);
-// module.exports.watchFolders = [
-//     path.resolve(__dirname, ".."),
+
+
+
+module.exports.watchFolders = [
+    // path.resolve(__dirname, ".."),
 //     // path.resolve(__dirname, "..", "node_modules"),
-//     // path.resolve(__dirname, "../packages", "api"),
-//     // path.resolve(__dirname, "../packages", "sdk-core"),
-//   ];
+    path.resolve(__dirname, "../packages", "api"),
+    path.resolve(__dirname, "../packages", "sdk-core"),
+  ];
 
   module.exports.resolver = { 
+    blockList: exclusionList([/sdk-core[\/\\]node_modules[/\\]react-native-reanimated[/\\].*/, /api[\/\\]node_modules[/\\]react-native-reanimated[/\\].*/]),
     alias: {
         "react": path.resolve('./node_modules/react'),
         "react-dom": path.resolve('./node_modules/react-dom'),
         "react-native": path.resolve('./node_modules/react-native')
-    }
+    },
+    extraNodeModules: new Proxy({}, {
+      get: (target, name) => path.join(process.cwd(), `../node_modules/${name}`),
+    }),
 }
 
 // // extraNodeModules: {
@@ -38,7 +46,5 @@ const path = require("path");
 // //   module.exports.resolver = { sourceExts: ['jsx', 'js', 'ts', 'tsx', 'cjs', 'json'] }
 
 // // module.exports.resolver = {
-// //     extraNodeModules: new Proxy({}, {
-// //       get: (target, name) => path.join(process.cwd(), `node_modules/${name}`),
-// //     }),
+
 // // };
